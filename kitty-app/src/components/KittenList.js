171998a6f1605bb48803d8532fa8f56df5f6324f
fay <wahomeimani@gmail.com>
2023-11-10 10:26:39 +0300

    commit-9

--- kitty-app/src/components/KittenList.js
+++ kitty-app/src/components/KittenList.js
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const KittenList = () => {
-  const [kittenImages, setKittenImages] = useState([]);
-
-  useEffect(() => {
-    const fetchKittenImages = async () => {
-      try {
-        const response = await fetch('https://placekitten.com', {
-          method: 'GET',
-          headers: {
-            // Add any headers if needed
-          },
-          // You can add other options like body, mode, etc.
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch kitten images');
-        }
-
-        const data = await response.blob(); // Assuming the response is an image, use blob()
-        setKittenImages(URL.createObjectURL(data));
-      } catch (error) {
-        console.error('Error fetching kitten images:', error.message);
-      }
-    };
-
-    fetchKittenImages();
-  }, []); // Empty dependency array to fetch data once when the component mounts
-
+const KittenList = ({ imageUrl }) => {
   return (
-    <div>
-      <h2>Cute Kitten Images</h2>
-      <img src={kittenImages} alt="Kitten" />
+    <div className="kitten-list">
+      <img src={imageUrl} alt="Kitten" />
     </div>
   );
 };
