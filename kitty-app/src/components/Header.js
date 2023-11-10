bc901915d4a3b7431ee1022123cd6d574f24176e
fay <wahomeimani@gmail.com>
2023-11-10 10:28:06 +0300

    commit-10

--- kitty-app/src/components/Header.js
+++ kitty-app/src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './Header.css';
 
 const Header = () => {
   return (
-    <header className="app-header">
-      <h1>Kitten App</h1>
+    <header>
+      <h1>Kitty App</h1>
     </header>
   );
 };
+
+
+
 
 export default Header;
